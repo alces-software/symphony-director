34ee7298f120908b1d32b23418ebe813ca4bd5e7
Steve Norledge <steve@honeydew.local>
2016-07-10 13:28:58 +0100

    - prepare puppet modules before execution

--- install/bin/stage3.sh
+++ install/bin/stage3.sh
@@ -15,6 +15,7 @@ fi
 ############# START PUPPET ###################
 if [ $PUPPET -gt 0 ]; then
   /opt/symphony/director/puppet/bin/install_all.sh
+  /opt/symphony/director/puppet/bin/prepare_modules.sh
   systemctl restart httpd
   puppet agent -t --environment=symphony
 fi
