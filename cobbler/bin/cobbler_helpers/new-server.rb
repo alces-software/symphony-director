39a7514ae3ce69eff29994c156b7b20a4373e976
Andrew Provis <andrew_provis@hotmail.com>
2016-07-25 16:53:18 +0100

    Initial setup of ruby script

---
+++ cobbler/bin/cobbler_helpers/new-server.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+=begin
+
+This script is to automate generating several cobbler systems in batch with ease
+
+=end
+
+
+puts '                                                        '
+puts '                                                        '
+puts '       `.:/+/                                           '
+puts '   ./oooo+`                                             '
+puts ' `/oooooo.                                              '
+puts ' /oooooo/                                               '
+puts ' ooooooo-  ./o/                                         '
+puts ' +oooooo/`+ooo                                          '
+puts ' -oooooooooooo                                          '
+puts '  :ooooooooooo. `:+:`                                   '
+puts '   -+ooooooooo+:ooo`                                    '
+puts '    `:ooooooooooooo.                               `.   '
+puts '      `:+oooooooooo+`                              /o/  '
+puts '        `-+ooooooooo+-                 :.   .+/   -ooo: '
+puts '           .:+oooooooo+-`            .+o: `:ooo  :oooo+ '
+puts '              `-/oooooooo/..-....-:/+ooo//oooo+/+ooooo/ '
+puts '                  ./oooooooo+oooooooooooooooooooooooo/` '
+puts '                    .+oooooooooo++oooooooooooooooo+:.   '
+puts '                      .:/+ooooooo-`..--::::::::-.`      '
+puts '                            `-:/oo+                     '
+puts '                               `-.                      '
+puts '                                                        '
+puts '                                                        '
+
+
