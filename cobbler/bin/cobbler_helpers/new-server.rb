88cd7632944a8580edca8f71d4b1b50c47f4446c
Andrew Provis <andrew_provis@hotmail.com>
2016-07-25 16:54:13 +0100

    Adding code to collect parameters

--- cobbler/bin/cobbler_helpers/new-server.rb
+++ cobbler/bin/cobbler_helpers/new-server.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 
+# Listing required resources
+# require 'open3'
+
 =begin
 
 This script is to automate generating several cobbler systems in batch with ease
@@ -30,5 +33,26 @@ puts '                            `-:/oo+                     '
 puts '                               `-.                      '
 puts '                                                        '
 puts '                                                        '
+
+
+
+
+
+
+# Parsing parameters
+
+# - Node type (login, master, hpcnode, etc...)
+# - Quantity
+# - Base name
+# - IP quad 3 range
+# - IP quad 4 range
+
+node_type = ARGV[0]
+quantity = ARGV[1]
+base_name = ARGV[2]
+ip_quad_3_range_low = ARGV[4]
+ip_quad_3_range_high = ARGV[5]
+ip_quad_4_range_low = ARGV[6]
+ip_quad_4_range_high = ARGV[7]
 
 
